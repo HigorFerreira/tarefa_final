5a7980f8764cf5ddfebee1d15f4af1bb7857097f
joaovbr0 <61693100+joaovbr0@users.noreply.github.com>
2023-06-03 11:25:01 -0300

    cript update

--- start.ps1
+++ start.ps1
@@ -1,6 +1,6 @@
 # Define as variáveis de ambiente
 # Solicitar ao usuário as informações do banco de dados
-$host = Read-Host -Prompt 'Insira o host do banco de dados'
+$_host = Read-Host -Prompt 'Insira o host do banco de dados'
 $port = Read-Host -Prompt 'Insira a porta do banco de dados'
 $user = Read-Host -Prompt 'Insira o nome de usuário do banco de dados'
 $password = Read-Host -Prompt 'Insira a senha do banco de dados' -AsSecureString
@@ -10,7 +10,8 @@ $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
 $password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
 # Definir as variáveis de ambiente
-$env:DB_HOST = $host
+$env:NODE_ENV = "development"
+$env:DB_HOST = $_host
 $env:DB_PORT = $port
 $env:DB_USER = $user
 $env:DB_PASSWORD = $password
